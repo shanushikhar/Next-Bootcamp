188f4ee2e87a67cfbcf77b911fb4d29d107d7c1b
Shanu Shikhar <thisisshanushikhar@gmail.com>
2022-11-09 23:08:43 +0530

    added dynamic routed & paths

--- pages/portfolio/[projectid].js
+++ pages/portfolio/[projectid].js
@@ -1,6 +1,11 @@
-import React from "react";
+import { useRouter } from "next/router";
 
 export default function projectid() {
+  const router = useRouter();
+  // console.log({ router });
+  console.log(router.query);
+  console.log(router.pathname);
+
   return <div>project id page</div>;
 }
 // http://localhost:3000/portfolio/22dfsds
