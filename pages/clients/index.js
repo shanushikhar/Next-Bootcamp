d83d401b0b662bc725d9d46e5acd381c78e07f77
Shanu Shikhar <thisisshanushikhar@gmail.com>
2022-11-10 01:04:16 +0530

    Navigating programatically

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,29 @@
-import React from "react";
+import Link from "next/link";
 
 export default function index() {
-  return <div>Client page</div>;
+  const blogs = [
+    { id: "leo", name: "leonardo" },
+    { id: "tyrian", name: "lannister" },
+  ];
+
+  return (
+    <div>
+      Client page
+      <div>
+        <ul>
+          {blogs.map((blog) => (
+            <li key={blog.id}>
+              <Link
+                href={`clients/${Math.floor(Math.random() * 10)}/${blog.id}`}
+              >
+                {blog.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 // http://localhost:3000/clients
